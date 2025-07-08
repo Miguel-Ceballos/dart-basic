a4eaa9c2cb11892bbaf8466c2f7004279e261ecd
Miguel Ceballos <mceballosleonides@gmail.com>
2025-07-07 21:46:05 -0600

    agrega name_constructor

--- 06_name_constructors.dart
+++ 06_name_constructors.dart
@@ -1,10 +1,19 @@
 void main() {
-
-  final rawJson = {
-    'name': 'To'
+  final Map<String, dynamic> rawJson = {
+    'name': 'Spiderman',
+    'power': 'Money',
+    'isAlive': true,
   };
 
-  final Hero ironman = Hero(name: 'Tony Stark', power: 'Money', isAlive: false);
+  // final Hero ironman = Hero(name: 'Tony Stark', power: 'Money', isAlive: false);
+
+  // final ironman = Hero(
+  //     isAlive: rawJson['isAlive2'] ?? false,
+  //     power: 'Money',
+  //     name: 'Tony Stark',
+  // );
+
+  final ironman = Hero.fromJson(rawJson);
   print(ironman);
 }
 
@@ -14,6 +23,11 @@ class Hero {
   bool isAlive;
 
   Hero({required this.name, this.power = 'sin poder', required this.isAlive});
+
+  Hero.fromJson(Map<String, dynamic> json)
+    : name = json['name'] ?? 'No name found',
+      power = json['power'] ?? 'No power found',
+      isAlive = json['isAlive'] ?? false;
 
   @override
   String toString() {
